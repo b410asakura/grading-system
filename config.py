e54192747b17bf037114dd548efbeba0eb37c027
luianqi <1ayanabdrahmanova1@gmail.com>
2022-05-17 18:46:28 +0600

    TEST

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-
+# import os
+#
+# DATABASE_URL = os.environ['DATABASE_URL']
+#
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+#
+#
