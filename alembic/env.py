43aa5883bddcc7ab222b295a1f28713d05eca35c
luianqi <1ayanabdrahmanova1@gmail.com>
2022-05-17 17:06:48 +0600

    added engine

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -18,6 +18,11 @@ sys.path.append(BASE_DIR)
 config = context.config
 
 config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+engine = create_engine(DATABASE_URL)
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
